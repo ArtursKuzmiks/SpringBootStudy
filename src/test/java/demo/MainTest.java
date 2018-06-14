90de54a401cc1360f654ac7fc9e42639e07fbcc6
Artur Kuzmik <arturs.kuzmiks@gmail.com>
2018-06-14 11:02:17 +0300

    latest build

--- src/test/java/demo/MainTest.java
+++ src/test/java/demo/MainTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {Main.class, AppTestConfig.class})
+@TestPropertySource(locations = "classpath:applicationTEST.properties")
 @ActiveProfiles("test")
 @Transactional
 public class MainTest {
